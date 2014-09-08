21ae8c496e5e604e4c9e5452dc7d9c88f352a66d
Arthur Benemann <arthur.benemann@gmail.com>
2014-09-07 22:05:09 -0700

    More compatibility to xboard

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
 #include "chessEngine.h"
  
 int main() {
-	signal(SIGINT,SIG_IGN);
+	char buf[256];
+	do{
+		fgets(buf, 256, stdin);
+		printf("# received %s\n", buf );
+		fflush(stdout);
+	}while(strcmp(buf,"protover 2\n")!=0);
+	
+	printf("feature sigint=0 done=1\n");
+	fflush(stdout);
+
 	chessInit();
 
 	while(1){
